a27ab4b54f06e49c99f2f5e9399d86b90b615761
maeta187 <nexus18789@gmail.com>
2023-09-23 17:45:03 +0900

    ✨ : Signinページの作成

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,11 @@
+import SigninForm from '@/app/signin/_components/SigninForm'
+
+export default function Page() {
+  return (
+    <div>
+      <div className='mx-auto my-0 w-10/12'>
+        <SigninForm />
+      </div>
+    </div>
+  )
+}
