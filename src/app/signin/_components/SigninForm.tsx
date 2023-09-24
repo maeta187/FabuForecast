1fd90f9c8a777607a3c0605dac15dced420d4162
maeta187 <nexus18789@gmail.com>
2023-09-24 16:51:22 +0900

    ✨ : SigninFormの作成

--- src/app/signin/_components/SigninForm.tsx
+++ src/app/signin/_components/SigninForm.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 const SigninForm = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
   return (
-    <form>
+    <form
+      className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-12 shadow-2xl'
+      onSubmit={handleSubmit}
+    >
       <h1>SigninForm</h1>
-      <div>
-        <div className='block'>
+      <div className='flex flex-col'>
+        <div>
           <label htmlFor='user-id' className=''>
             ユーザーID
           </label>
@@ -12,9 +21,60 @@ const SigninForm = () => {
         <input
           id='user-id'
           type='text'
+          autoComplete='off'
           placeholder='ユーザーID'
           className='input input-bordered input-accent w-full max-w-xs'
         />
+      </div>
+      <div className='mt-6 flex flex-col'>
+        <div>
+          <label htmlFor='email' className=''>
+            メールアドレス
+          </label>
+          <span className='text-xs text-error'>※必須</span>
+        </div>
+        <input
+          id='email'
+          type='email'
+          autoComplete='email'
+          placeholder='example@example.com'
+          className='input input-bordered input-accent w-full max-w-xs'
+        />
+      </div>
+      <div className='mt-6 flex flex-col'>
+        <div>
+          <label htmlFor='password' className=''>
+            パスワード
+          </label>
+          <span className='text-xs text-error'>※必須</span>
+        </div>
+        <input
+          id='password'
+          type='password'
+          autoComplete='new-password'
+          placeholder='パスワードを入力してください'
+          className='input input-bordered input-accent w-full max-w-xs'
+        />
+      </div>
+      <div className='mt-6 flex flex-col'>
+        <div>
+          <label htmlFor='password-confirm' className=''>
+            パスワード（確認用）
+          </label>
+          <span className='text-xs text-error'>※必須</span>
+        </div>
+        <input
+          id='password-confirm'
+          type='password'
+          autoComplete='new-password'
+          placeholder='パスワードを再入力してください'
+          className='input input-bordered input-accent w-full max-w-xs'
+        />
+      </div>
+      <div className='mt-6 text-center'>
+        <button className='btn btn-primary px-10' type='submit'>
+          登録
+        </button>
       </div>
     </form>
   )
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,7 +3,7 @@ import SigninForm from '@/app/signin/_components/SigninForm'
 export default function Page() {
   return (
     <div>
-      <div className='mx-auto my-0 w-10/12'>
+      <div className='relative mx-auto my-0 h-screen w-10/12'>
         <SigninForm />
       </div>
     </div>
