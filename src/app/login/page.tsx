efda86533e12d922caf8508cf4424f28fd1ee56c
maeta187 <nexus18789@gmail.com>
2023-09-26 22:35:41 +0900

    ✨ : Signinページの作成

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from '@/app/login/_components/LoginForm'
+
+export default function Page() {
+  return (
+    <div>
+      <div className='relative mx-auto my-0 h-screen w-10/12'>
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
