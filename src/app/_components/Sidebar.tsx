e77dd9a3b5e611173576088c0f28f26d5efa7bcf
maeta187 <nexus18789@gmail.com>
2023-09-26 23:39:46 +0900

    🚧 : Sidebar/ベースUIの実装

---
+++ src/app/_components/Sidebar.tsx
@@ -0,0 +1,17 @@
+const Sidebar = () => {
+  return (
+    <div className='drawer-side'>
+      <label htmlFor='my-drawer-3' className='drawer-overlay'></label>
+      <ul className='menu min-h-full w-80 bg-base-200 p-4'>
+        <li>
+          <a>Sidebar Item 1</a>
+        </li>
+        <li>
+          <a>Sidebar Item 2</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
