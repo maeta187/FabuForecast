2a597cb649209528cd5ba038b0d20b445cd3b20d
maeta187 <nexus18789@gmail.com>
2023-09-27 22:29:58 +0900

    ✨ : Sidebarの実装

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -1,13 +1,16 @@
+import Link from 'next/link'
+
 const Sidebar = () => {
   return (
     <div className='drawer-side'>
       <label htmlFor='my-drawer-3' className='drawer-overlay'></label>
       <ul className='menu min-h-full w-80 bg-base-200 p-4'>
+        {/* TODO: ログイン機能実装後セッションデータをもとにナビバーを切り替えるようにする */}
         <li>
-          <a>Sidebar Item 1</a>
+          <Link href='/signin'>サインイン</Link>
         </li>
         <li>
-          <a>Sidebar Item 2</a>
+          <Link href='/login'>ログイン</Link>
         </li>
       </ul>
     </div>
