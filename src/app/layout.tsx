f218282cc96f930678e5ef0691852b16dd098ecc
maeta187 <nexus18789@gmail.com>
2023-10-03 22:40:11 +0900

    💡 : NextAuthProviderをコメントして配置

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '@/app/_components/Header'
 import SideBar from '@/app/_components/Sidebar'
+// NOTE: ページ全体でセッション情報を使う場合はコメントアウトを外す
+// import { NextAuthProvider } from '@/contexts/SessionContext'
 import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang='ja'>
       <body className={inter.className}>
+        {/* <NextAuthProvider> */}
         <div className='drawer'>
           <input id='my-drawer-3' type='checkbox' className='drawer-toggle' />
           <div className='drawer-content flex flex-col'>
@@ -27,6 +30,7 @@ export default function RootLayout({
           </div>
           <SideBar />
         </div>
+        {/* </NextAuthProvider> */}
       </body>
     </html>
   )
