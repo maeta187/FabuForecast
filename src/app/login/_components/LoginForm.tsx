de6f4c6c41a6fc1bdfc04b50b70f274c9a967646
maeta187 <nexus18789@gmail.com>
2023-10-08 00:21:39 +0900

    🦺 : LoginFormへバリデーションの実装

--- src/app/login/_components/LoginForm.tsx
+++ src/app/login/_components/LoginForm.tsx
@@ -1,17 +1,30 @@
 'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { loginFormSchema, loginFormData } from '@/utils/validations/schema'
 
 const LoginForm = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const handleOnSubmit: SubmitHandler<loginFormData> = (data) => {
+    console.log(data)
   }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors: formatError, isValid, isSubmitting }
+  } = useForm<loginFormData>({
+    mode: 'onBlur',
+    resolver: zodResolver(loginFormSchema)
+  })
 
   return (
     <form
       className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-12 shadow-2xl'
-      onSubmit={handleSubmit}
+      onSubmit={(event) => {
+        void handleSubmit(handleOnSubmit)(event)
+      }}
     >
-      <h1>LoginForm</h1>
-      <div className='mt-6 flex flex-col'>
+      <div className='flex w-80 flex-col'>
         <div>
           <label htmlFor='email' className=''>
             メールアドレス
@@ -24,9 +37,15 @@ const LoginForm = () => {
           autoComplete='email'
           placeholder='example@example.com'
           className='input input-bordered input-accent w-full max-w-xs'
+          {...register('email')}
         />
+        {formatError.email && (
+          <span className='mt-1 text-xs text-error'>
+            {formatError.email.message}
+          </span>
+        )}
       </div>
-      <div className='mt-6 flex flex-col'>
+      <div className='mt-6 flex w-80 flex-col'>
         <div>
           <label htmlFor='password' className=''>
             パスワード
@@ -39,10 +58,20 @@ const LoginForm = () => {
           autoComplete='new-password'
           placeholder='パスワードを入力してください'
           className='input input-bordered input-accent w-full max-w-xs'
+          {...register('password')}
         />
+        {formatError.password && (
+          <span className='mt-1 text-xs text-error'>
+            {formatError.password.message}
+          </span>
+        )}
       </div>
       <div className='mt-6 text-center'>
-        <button className='btn btn-primary px-10' type='submit'>
+        <button
+          className='btn btn-primary px-10'
+          type='submit'
+          disabled={!isValid || isSubmitting}
+        >
           ログイン
         </button>
       </div>
