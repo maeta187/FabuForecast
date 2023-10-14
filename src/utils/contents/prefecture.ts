0356eb19d7a02a0aa320d0fd66da746fefd79cb3
maeta187 <nexus18789@gmail.com>
2023-10-14 15:21:18 +0900

    🤡 : 都道府県データモックの作成

---
+++ src/utils/contents/prefecture.ts
@@ -0,0 +1,23 @@
+import { Prefecture } from '@/types'
+
+// TODO: 完成したら他の都道府県も追加する
+export const prefectures: Prefecture[] = [
+  {
+    name: '東京都',
+    value: 'tokyo',
+    latitude: 35.6895,
+    longitude: 139.6917
+  },
+  {
+    name: '神奈川県',
+    value: 'kanagawa',
+    latitude: 35.4708,
+    longitude: 139.6336
+  },
+  {
+    name: '広島県',
+    value: 'hiroshima',
+    latitude: 34.4,
+    longitude: 132.45
+  }
+]
