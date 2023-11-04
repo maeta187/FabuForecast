343e950cfb805d7bee8535a84a17c2816bdad858
maeta187 <nexus18789@gmail.com>
2023-11-04 14:57:07 +0900

    ✨ : login/ToastProviderの設置

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
 import LoginForm from '@/app/login/_components/LoginForm'
+import { ToastProvider } from '@/contexts/ToastContext'
 
 export default function Page() {
   return (
     <div>
       <div className='relative mx-auto my-0 h-screen w-10/12'>
-        <LoginForm />
+        <ToastProvider>
+          <LoginForm />
+        </ToastProvider>
       </div>
     </div>
   )
