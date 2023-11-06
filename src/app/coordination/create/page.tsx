88edff174539a20f749fcc92a0f97d794c75d3f0
maeta187 <nexus18789@gmail.com>
2023-11-06 23:10:02 +0900

    🚧  : コーディネート登録ページの作成

---
+++ src/app/coordination/create/page.tsx
@@ -0,0 +1,21 @@
+import CreateForm from '@/app/coordination/_components/CreateForm'
+import { ResponseForecast } from '@/types'
+import { forecastDataProcessing } from '@/utils'
+
+export default async function Page() {
+  const baseUrl = process.env.NEXTAUTH_URL
+  const response = await fetch(`${baseUrl}/api/forecast`, {
+    cache: 'no-cache'
+  })
+  const forecast: ResponseForecast = await response.json()
+  if (!forecast) return
+  const forecastData = forecastDataProcessing(forecast)
+  console.log(forecastData)
+  return (
+    <div>
+      <div className='relative mx-auto my-0 h-screen w-10/12'>
+        <CreateForm />
+      </div>
+    </div>
+  )
+}
